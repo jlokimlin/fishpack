24b9367b060fb40be88de9042d117cde25748ee0
jlokimlin <jlokimlin@gmail.com>
2016-03-19 12:49:21 -0700

    Added a separate Poisson solver

---
+++ src/type_PoissonSolver.f90
@@ -0,0 +1,90 @@
+module type_PoissonSolver
+
+    use, intrinsic :: iso_fortran_env, only: &
+        wp => REAL64, &
+        ip => INT32
+
+    use type_HelmholtzSolver, only: &
+        HelmholtzSolver
+
+    ! Explicit typing only
+    implicit none
+
+    ! Everything is private unless stated otherwise
+    private
+    public :: PoissonSolver
+
+    ! Declare derived data type
+    type, extends( HelmholtzSolver ), public :: PoissonSolver
+        !---------------------------------------------------------------------------------
+        ! Class variables
+        !---------------------------------------------------------------------------------
+    contains
+        !---------------------------------------------------------------------------------
+        ! Class methods
+        !---------------------------------------------------------------------------------
+        procedure, non_overridable, public :: solve_2d_poisson_centered
+        procedure, non_overridable, public :: solve_2d_poisson_staggered
+        final                              :: finalize_poisson_solver
+        !---------------------------------------------------------------------------------
+    end type PoissonSolver
+
+
+contains
+
+
+    subroutine solve_2d_poisson_centered( this, source_term, solution )
+        !
+        !--------------------------------------------------------------------------------
+        ! Dictionary: calling arguments
+        !--------------------------------------------------------------------------------
+        class (PoissonSolver),  intent (in out) :: this
+        real (wp), contiguous,  intent (in out) :: source_term(:,:)
+        real (wp), contiguous,  intent (out)    :: solution(:,:)
+        !--------------------------------------------------------------------------------
+
+        ! Ensure that object is usable
+        call this%destroy()
+
+        ! Solve poisson's equation
+        associate( helmholtz_constant => 0.0_wp )
+            call this%solve_2d_helmholtz_centered( helmholtz_constant, source_term, solution )
+        end associate
+
+    end subroutine solve_2d_poisson_centered
+
+
+    subroutine solve_2d_poisson_staggered( this, source_term, solution )
+        !
+        !--------------------------------------------------------------------------------
+        ! Dictionary: calling arguments
+        !--------------------------------------------------------------------------------
+        class (PoissonSolver),  intent (in out) :: this
+        real (wp), contiguous,  intent (in out) :: source_term(:,:)
+        real (wp), contiguous,  intent (out)    :: solution(:,:)
+        !--------------------------------------------------------------------------------
+
+        ! Ensure that object is usable
+        call this%destroy()
+
+        ! Solve poisson's equation
+        associate( helmholtz_constant => 0.0_wp )
+            call this%solve_2d_helmholtz_staggered( helmholtz_constant, source_term, solution )
+        end associate
+
+    end subroutine solve_2d_poisson_staggered
+
+
+    subroutine finalize_poisson_solver( this )
+        !--------------------------------------------------------------------------------
+        ! Dictionary: calling arguments
+        !--------------------------------------------------------------------------------
+        type (PoissonSolver), intent (in out) :: this
+        !--------------------------------------------------------------------------------
+
+        call this%destroy()
+
+    end subroutine finalize_poisson_solver
+
+
+end module type_PoissonSolver
